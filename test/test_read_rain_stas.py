52dcceafc4a0d9632841739704c0602a451cf1d2
forestbat <k678900000@163.com>
2023-10-11 18:57:37 +0800

    update requirements.txt

--- test/test_read_rain_stas.py
+++ test/test_read_rain_stas.py
@@ -11,13 +11,14 @@ from hydromodel.utils.dmca_esr import step1_step2_tr_and_fluctuations_timeseries
     step7_end_flow_events, step9_checks_on_flow_events, step10_checks_on_overlapping_events
 from pandas import DataFrame
 from shapely import Point
-from xaj.calibrate_ga_xaj_bmi import calibrate_by_ga
+from xaj.calibrate_ga_xaj_bmi import calibrate_by_ga  # noqa:401
 
 import definitions
 
-
 gdf_biliu_shp: GeoDataFrame = gpd.read_file(os.path.join(definitions.ROOT_DIR, 'example/biliuriver_shp'
-                                                                                   '/碧流河流域.shp'), engine='pyogrio')
+                                                                               '/碧流河流域.shp'), engine='pyogrio')
+
+
 class my:
     data_dir = '.'
 
@@ -91,7 +92,7 @@ def get_rain_average():
 def get_infer_inq():
     biliu_flow_df = pd.read_csv(os.path.join(definitions.ROOT_DIR, 'example/biliuriver_rsvr.csv'),
                                 engine='c', parse_dates=['TM'])
-    biliu_area = gdf_biliu_shp.geometry[0].area*12100
+    biliu_area = gdf_biliu_shp.geometry[0].area * 12100
     biliu_flow_df: DataFrame = biliu_flow_df.fillna(-1)
     inq_array = biliu_flow_df['INQ'].to_numpy()
     otq_array = biliu_flow_df['OTQ'].to_numpy()
@@ -106,7 +107,7 @@ def get_infer_inq():
     new_df = pd.DataFrame({'TM': tm_array, 'INQ': inq_array, 'OTQ': otq_array})
     new_df = new_df.set_index('TM').resample('H').interpolate()
     # 流量单位转换
-    new_df['INQ_mm/h'] = new_df['INQ'].apply(lambda x: x*3.6/biliu_area)
+    new_df['INQ_mm/h'] = new_df['INQ'].apply(lambda x: x * 3.6 / biliu_area)
     new_df.to_csv(os.path.join(definitions.ROOT_DIR, 'example/biliu_inq_interpolated.csv'))
     return new_df['INQ'].to_numpy(), new_df['INQ_mm/h'].to_numpy(), new_df.index.to_numpy()
 
@@ -152,20 +153,32 @@ def test_biliu_rain_flow_division():
         flow_session_mm_h = flow_mm_h[start:end + 1]
         flow_session_m3_s = flow_m3_s[start:end + 1]
         rain_time = time[start:end + 1]
-        session_df = pd.DataFrame({'RAIN_TM': rain_time, 'RAIN_SESSION': rain_session, 'FLOW_SESSION_MM_H': flow_session_mm_h, 'FLOW_SESSION_M3_S':flow_session_m3_s})
-        date_path = np.datetime_as_string(rain_time[0]).split('T')[0] + np.datetime_as_string(rain_time[0]).split('T')[1].split(':')[0] + np.datetime_as_string(rain_time[0]).split('T')[1].split(':')[1]
+        session_df = pd.DataFrame(
+            {'RAIN_TM': rain_time, 'RAIN_SESSION': rain_session, 'FLOW_SESSION_MM_H': flow_session_mm_h,
+             'FLOW_SESSION_M3_S': flow_session_m3_s})
+        date_path = np.datetime_as_string(rain_time[0]).split('T')[0] + \
+                    np.datetime_as_string(rain_time[0]).split('T')[1].split(':')[0] + \
+                    np.datetime_as_string(rain_time[0]).split('T')[1].split(':')[1]
         session_df.to_csv(os.path.join(definitions.ROOT_DIR, 'example/sessions/' +
                                        date_path + '_session.csv'))
-    return BEGINNING_RAIN, END_RAIN, BEGINNING_FLOW, END_FLOW
 
 
+# need fusion with the last test
 def test_calibrate_flow():
+    # pet_df含有潜在蒸散发
+    pet_df = pd.read_csv(os.path.join(definitions.ROOT_DIR, 'example/era5_data/pet_calc/PET_result.CSV'), engine='c',
+                         parse_dates=['time']).set_index('time')
     session_path = os.path.join(definitions.ROOT_DIR, 'example/sessions/')
     for dir_name, sub_dir, files in os.walk(session_path):
         for file in files:
-            session_df = pd.read_csv(file, engine='c')
-            # calibrate_by_ga()
-
+            # session_df 含有雨和洪
+            session_df = pd.read_csv(os.path.join(session_path, file), engine='c', parse_dates=['RAIN_TM']).set_index(
+                'RAIN_TM').drop(columns=['Unnamed: 0'])
+            session_pet = pet_df.loc[session_df.index].to_numpy().flatten()
+            calibrate_df = pd.DataFrame({'PRCP': session_df['RAIN_SESSION'].to_numpy(), 'PET': session_pet,
+                                         'streamflow': session_df['FLOW_SESSION_M3_S'].to_numpy()})
+            calibrate_np = calibrate_df.to_numpy()
+            # calibrate_by_ga(input=calibrate_np)
 
 
 def get_voronoi():
